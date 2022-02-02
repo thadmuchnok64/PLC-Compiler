6300936a953e7b210d7cf6429846c7ce2bb47f43
thadmuchnok64 <tmuchnok@ufl.edu>
2022-02-01 22:54:45 -0500

    uhh

--- Lexer.java
+++ Lexer.java
@@ -2,12 +2,18 @@ import java.util.ArrayList;
 
 public class Lexer implements ILexer {
     
+    ArrayList<ArrayList<Token>> tokens;
     ArrayList<ArrayList<Character>> chars;
-    
+
+
     private enum State {START, IN_IDENT, HAVE_ZERO, HAVE_DOT, IN_FLOAT, IN_NUM, HAVE_EQ, HAVE_MINUS};
     private State state;
     public Lexer Lexer(String input)
     {
+        char[] chars = input.toCharArray();
+        for(char c : chars){
+            
+        }
         state = State.START;
         LexerTime(input);
     }
@@ -20,4 +26,6 @@ public class Lexer implements ILexer {
         }
     }
 
+
+    
 }
