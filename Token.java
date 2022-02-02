4696a1c341d20f9fb94a732473808571e0bfd90f
thadmuchnok64 <tmuchnok@ufl.edu>
2022-02-01 19:48:58 -0500

    getIntValue()

--- Token.java
+++ Token.java
@@ -6,11 +6,27 @@ public class Token implements IToken
     Kind kind;
     String input;
 
-@Override
 
 // Returns the kind - TM
+@Override
 public Kind getKind() {
     return kind;
+}
+
+// Returns the text of the token - TM
+@Override
+public String getText() {
+    return input;
+}
+
+// Returns the Integer value of the token
+@Override
+public int getIntValue() {
+    if(kind==Kind.INT_LIT){
+        return Integer.parseInt(getText());
+    } else {
+        throw new NumberFormatException();
+    }
 }
 
 }
