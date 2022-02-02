d9fa3132a16d3bb20dab70ad5e0b0393b4bfa9b5
TheFHazelnut <02enzio@gmail.com>
2022-02-01 23:50:31 -0500

    Switcharoo Statementpoo

--- Lexer.java
+++ Lexer.java
@@ -1,24 +1,51 @@
 import java.util.ArrayList;
 
+import edu.ufl.cise.plc.IToken.Kind;
+
 public class Lexer implements ILexer {
     
-    ArrayList<ArrayList<Token>> tokens;
+    ArrayList<Token> tokens;
     ArrayList<ArrayList<Character>> chars;
     int pos = 0;
-
+    int line = 0;
 
     private enum State {START, IN_IDENT, HAVE_ZERO, HAVE_DOT, IN_FLOAT, IN_NUM, HAVE_EQ, HAVE_MINUS};
     private State state;
     public Lexer Lexer(String input)
     {
-        char[] chars = input.toCharArray();
-        for(char c : chars){
+        char[] charArray = input.toCharArray();
+        for(char c: charArray)
+        {
+            switch(c)
+            {
+                case ' ', '\t', '\n', '\r':
+                    line++;
+                    chars.get(line).add(c);
+                    break;
+                default:
+                    chars.get(line).add(c);
+                    break;
+            }
+        }
 
-            char ch = chars[pos];
+
+        for(char c : charArray){
+
+            char ch = charArray[pos];
+            int startPos;
             switch(state)
             {
                 case START:
-
+                    startPos = pos;
+                    switch(ch)
+                    {
+                        case ' ', '\t', '\n', '\r' :
+                            {pos++;}
+                        break;
+                        
+                        case '+':
+                            Token newToken = new Token(Kind.PLUS, ch, pos, 1, _sourceLocation)
+                    }
                 break;
                 case IN_IDENT:
                 break;
