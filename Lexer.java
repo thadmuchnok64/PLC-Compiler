c85c2553f9b9fee04fdc348a3d8b687b2c8e318f
TheFHazelnut <02enzio@gmail.com>
2022-02-01 22:55:07 -0500

    Lexy Wexy

--- Lexer.java
+++ Lexer.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Lexer implements ILexer {
     
     ArrayList<ArrayList<Character>> chars;
-    
+    int pos = 0;
+
     private enum State {START, IN_IDENT, HAVE_ZERO, HAVE_DOT, IN_FLOAT, IN_NUM, HAVE_EQ, HAVE_MINUS};
     private State state;
     public Lexer Lexer(String input)
@@ -16,7 +17,26 @@ public class Lexer implements ILexer {
     {
         while(true)
         {
-            
+            char ch = chars.get(pos);
+            switch(state)
+            {
+                case START:
+                break;
+                case IN_IDENT:
+                break;
+                case HAVE_ZERO:
+                break;
+                case HAVE_DOT:
+                break;
+                case IN_FLOAT:
+                break;
+                case IN_NUM:
+                break;
+                case HAVE_EQ:
+                break;
+                case HAVE_MINUS:
+                break;
+            }
         }
     }
 
