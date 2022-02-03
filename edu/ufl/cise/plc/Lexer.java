38aa211c8d409597d98265dd0fc788f06a7b72f1
thadmuchnok64 <tmuchnok@ufl.edu>
2022-02-02 20:57:57 -0500

    More stuff for the gigantic switch statement

--- edu/ufl/cise/plc/Lexer.java
+++ edu/ufl/cise/plc/Lexer.java
@@ -49,24 +49,28 @@ public class Lexer implements ILexer {
             }
         }
 
+        String str = "";
+        State currentState = State.START;
 
         for(int i = 0; i < chars.size(); i++)
             for(int j = 0; j < chars.get(i).size(); j++){
 
             char ch = chars.get(i).get(j);
-            String str = "";
             int startPos;
             Token newToken;
+            boolean endScan = true;
 
             //test switch - TM
-                State currentState = State.START;
                 switch(ch){
                     //Check for ident starter
                     case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_':
-                        if(currentState!=State.IN_IDENT){
-                            //do stuff here
-                        }
+                        if(currentState!=State.IN_IDENT&&currentState!=State.START){
+                            break;
+                        } else {
                         currentState = State.IN_IDENT;
+                        str = str + ch;
+                        endScan = false;
+                        }
                     break;
                     // Check for Num
                     case '0','1','2','3','4','5','6','7','8','9':
@@ -76,20 +80,48 @@ public class Lexer implements ILexer {
                             break;
                             default:
                             currentState=State.IN_NUM;
+                            str = str + ch;
+                            endScan = false;
                             break;
                         }
-                         
                     break;
                     case '.':
                     //check for Float
-                    if(currentState == state.HAVE_ZERO||currentState==state.IN_NUM){
-                        currentState = State.HAVE_DOT;
-                    } else {
+                    if(currentState == State.HAVE_ZERO||currentState==State.IN_NUM){
                         currentState = State.IN_FLOAT;
+                        str = str + ch;
+                        endScan = false;
+                    } else if(currentState==State.START) {
+                        currentState = State.HAVE_ZERO;
+                        str = str + ch;
+                        endScan = false;
                     }
                     break;
-                    case '\b','\t','\n','\f','\r','"','\'','\\':
+                    case '=':
+                        if(currentState==State.START){
+                            currentState= State.HAVE_EQ;
+                        } else if(currentState==State.HAVE_EQ){
+                            str = str + ch;
+                            endScan = false;
+                        }
                     break;
+                    case '-':
+                    if(currentState==State.START){
+                        currentState= State.HAVE_MINUS;
+                    } else if(currentState==State.HAVE_MINUS){
+                        str = str + ch;
+                        endScan = false;
+                    }
+                    break;
+                    case '\b','\t','\n','\f','\r','"','\'','\\',' ':
+                        if(currentState==State.START){
+                            //Skip white space if nothing is scanned yet.
+                            endScan = false;
+                        }
+                    break;
+                    default:
+                        throw new UnsupportedOperationException("oopsie poopsie, looks like you made an invalid term.");
+                    
                 }
 
             // end of test
@@ -137,10 +169,10 @@ public class Lexer implements ILexer {
                 }
                 case HAVE_ZERO:
                 break;
-                //case HAVE_DOT:
-                //break;
-                //case IN_FLOAT:
-                //break;
+                case HAVE_DOT:
+                break;
+                case IN_FLOAT:
+                break;
                 case IN_NUM:
                 {
                     newToken = new Token(Kind.INT_LIT,ch+"",pos,1,i,j);
